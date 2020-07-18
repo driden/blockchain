60ee5bb1e90caafb52f3b1d4ca258c51f039d9a9
driden <lignacio.loureiro@gmail.com>
2020-07-18 18:19:13 -0300

    changes

--- contracts/obligatorio.sol
+++ contracts/obligatorio.sol
@@ -9,23 +9,23 @@ contract Inherit {
         string email;
         uint hireDate;
     }
-    
+
     struct Heir {
         address payable account;
         uint8 percentage;
         uint payoutOrder;
     }
-    
+
     uint public amountInheritance = address(this).balance;
-    
+
     uint public amountHeirs = 0;
     Heir[] public heirs;
-    
-    uint8 public amountInCharge = 0;
+
+    uint8 public amountManagers = 2;
     address[5] public managers;
-    
+
     Person public owner ;
-    
+
     //Debe recibir owner, un heredero y dos managers.
     constructor() public payable {
         owner = Person ({
@@ -44,7 +44,7 @@ contract Inherit {
         managers[0] = 0x5e41814b7A7ab6afcE148E9D0ae8D79e88259Fcf;
         managers[1] = 0x3155590fCA8867c0adCFDEe9c7b314408b290571;
     }
-    
+
     //Cada heredero debe ser designado con el porcentaje que debe recibir de herencia
     function addHeir (address payable heirAccount, uint8 heirPercentage, uint heirPayoutOrder) public{
         require(heirPayoutOrder > 0, "Payout order must be greater than 0");
@@ -55,14 +55,13 @@ contract Inherit {
             require(heirs[j].payoutOrder != heirPayoutOrder, "An heir already exists in that payout position.");
             percentage += heirs[j].percentage;
         }
-        
+
         require(percentage + heirPercentage <= 100, "Adding this heir with percentage would exceed 100 percent");
         heirs.push(Heir({account: heirAccount, percentage:heirPercentage, payoutOrder: heirPayoutOrder}));
         amountHeirs++;
     }
-    
+
     function removeHeir(address payable heirAccount) public {
-        
         require(heirs.length > 1, "Cannot remove the only heir");
         for (uint j = 0; j < heirs.length; j++) {
             if (heirs[j].account == heirAccount){
@@ -70,7 +69,30 @@ contract Inherit {
                 heirs[j] = heirs[heirs.length - 1];
                 heirs.length--;
             }
-            
+        }
+    }
+
+    // se puede agregar a manager un beneficiario?
+    function addManager (address payable managerAccount) public {
+        require(amountManagers < 5, "You can't add a new manager");
+
+        for(uint i = 0; i < amountManagers; i++) {
+            require(managers[i] != managerAccount, "this address is already listed as a manager");
+        }
+
+        managers[amountManagers] = managerAccount;
+        amountManagers++;
+    }
+
+    function removeManager(address payable managerAccount) public {
+        require(amountManagers > 2, "You can't remove a manager right now, a minimum of 2 is needed");
+
+        for (uint j = 0; j < managers.length; j++) {
+            if (managers[j] == managerAccount){
+                delete managers[j];
+                managers[j] = managers[amountManagers - 1];
+                amountManagers--;
+            }
         }
     }
 }
